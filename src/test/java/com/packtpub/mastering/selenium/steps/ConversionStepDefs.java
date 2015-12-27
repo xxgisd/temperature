ea27a2e509d5c11821cae6467964df3c97184104
Xiuli Gong <xxgisd@rit.edu>
2015-12-27 18:57:20 -0500

    initial commit

---
+++ src/test/java/com/packtpub/mastering/selenium/steps/ConversionStepDefs.java
@@ -0,0 +1,33 @@
+package com.packtpub.mastering.selenium.steps;
+
+import cucumber.annotation.en.Given;
+import cucumber.annotation.en.Then;
+import cucumber.annotation.en.When;
+import cucumber.runtime.PendingException;
+
+/**
+ * Created by julieGong on 12/27/15.
+ */
+public class ConversionStepDefs {
+
+
+    @Given("^I want to  convert (\\d+).(\\d+) degree Fahrenheit to Celsius$")
+    public void I_want_to_convert_degree_Fahrenheit_to_Celsius(int arg1, int arg2) throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @When("^I input the value of Fahrenheit as (\\d+).(\\d+) in text field$")
+    public void I_input_the_value_of_Fahrenheit_as_in_text_field(int arg1, int arg2) throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @Then("^It should be converted to Celsius as (\\d+) degree$")
+    public void It_should_be_converted_to_Celsius_as_degree(int arg1) throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+
+}
