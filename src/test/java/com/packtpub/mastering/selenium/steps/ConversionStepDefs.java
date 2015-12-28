7e99382d0404b7d8424c28e760e9ee46f76743e7
Xiuli Gong <xxgisd@rit.edu>
2015-12-27 19:01:18 -0500

    new changes

--- src/test/java/com/packtpub/mastering/selenium/steps/ConversionStepDefs.java
+++ src/test/java/com/packtpub/mastering/selenium/steps/ConversionStepDefs.java
@@ -20,6 +20,8 @@ public class ConversionStepDefs {
     @When("^I input the value of Fahrenheit as (\\d+).(\\d+) in text field$")
     public void I_input_the_value_of_Fahrenheit_as_in_text_field(int arg1, int arg2) throws Throwable {
         // Express the Regexp above with the code you wish you had
+
+        // new changes here
         throw new PendingException();
     }
 
